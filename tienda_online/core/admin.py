140e647defd39f2e3f66f80f4320ec96783e8ff0
Felipe Lopez Rios <felipetomaslopezrios@gmail.com>
2023-03-18 20:39:36 -0300

    Se crea panel de administracion y superusuario 'admin'

--- tienda_online/core/admin.py
+++ tienda_online/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from core.models import Cliente, Articulo, Pedido
 
 # Register your models here.
+
+admin.site.register(Cliente)
+admin.site.register(Articulo)
+admin.site.register(Pedido)
